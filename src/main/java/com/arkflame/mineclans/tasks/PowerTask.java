125f42e867ffb38cee00a9f24b756ff2413b2d86
linsaftw <linsaftw@users.noreply.github.com>
2025-05-22 23:26:56 -0300

    more power for negative players

--- src/main/java/com/arkflame/mineclans/tasks/PowerTask.java
+++ src/main/java/com/arkflame/mineclans/tasks/PowerTask.java
@@ -6,13 +6,18 @@ import org.bukkit.entity.Player;
 import com.arkflame.mineclans.MineClans;
 
 public class PowerTask implements Runnable {
-    private static int POWER_UPDATE_INTERVAL = 20 * 60 * 15; // 15 minutes
+    private static int POWER_UPDATE_INTERVAL = 20 * 60 * 2; // 2 minutes
     private static double POWER_PER_HOUR = 4; // Power to give per hour
 
     public static double getPowerPerUpdate(Player player) {
         double rankMultiplier = MineClans.getInstance().getPowerMultiplier(player);
+        double negativeMultiplier = hasNegativePower(player) ? 2 : 1;
         double multiplier = (double) POWER_UPDATE_INTERVAL / 20 / 60 / 60;
-        return POWER_PER_HOUR * multiplier * rankMultiplier;
+        return POWER_PER_HOUR * multiplier * rankMultiplier * negativeMultiplier;
+    }
+
+    private static boolean hasNegativePower(Player player) {
+        return MineClans.getInstance().getAPI().getFactionPlayer(player).getPower() < 0;
     }
 
     public void run() {
